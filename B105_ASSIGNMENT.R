190c24539b551fe39f41e850e1ffad9e23cb28ba
laptop-rt240j65\sambh <abhi95.cs118@gmail.com>
2026-09-11 19:01:42 +0200

    First Visualisation

--- B105_ASSIGNMENT.R
+++ B105_ASSIGNMENT.R
@@ -3,3 +3,18 @@ dim(Customer_Churn_Records)
 names(Customer_Churn_Records)
 str(Customer_Churn_Records)
 head(Customer_Churn_Records)
+
+colSums(is.na(Customer_Churn_Records))
+sum(duplicated(Customer_Churn_Records))
+
+summary(Customer_Churn_Records[, c("Age", "Balance", "Exited")])
+table(Customer_Churn_Records$Exited)
+prop.table(table(Customer_Churn_Records$Exited)) * 100
+
+barplot(
+  table(Customer_Churn_Records$Exited),
+  names.arg = c("Stayed", "Exited"),
+  main = "Customer Churn Distribution",
+  xlab = "Customer Status",
+  ylab = "Number of Customers"
+)
